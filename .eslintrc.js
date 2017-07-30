de25ef06c69df5e3f43839fef58c580acc330da8
Tyler Brown <tbrown9619@gmail.com>
2017-07-30 15:36:14 -0500

    starting to play around with validating route param and then displaying it

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,18 @@ module.exports = {
     'html'
   ],
   // add your custom rules here
-  rules: {},
+  rules: { // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    "space-before-function-paren": 0,
+
+    "semi": [2, "always"],
+
+    "indent": ["error", 4]
+  },
   globals: {}
 }
---
+++ pages/checklists/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       test {{ id }}
+    </div>
+</template>
+
+<script>
+export default {
+    validate({ params }) {
+        return /^\d+$/.test(params.id);
+    },
+    data: function () {
+        return {
+            id: this.$route.params.id
+        };
+    }
+};
+</script>
